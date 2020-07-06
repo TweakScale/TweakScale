1d007441069131874fd4a18795a94a4eebd3ccde
Lisias <me@lisias.net>
2020-07-06 01:17:57 -0300

    Removing Houston for KSP 1.10; Updating KSP Recall Houston to act only on KSP 1.9.

--- Source/Scale/GUI/UnsupportedKSPAlertBox.cs
+++ Source/Scale/GUI/UnsupportedKSPAlertBox.cs
@@ -6,20 +6,20 @@ namespace TweakScale.GUI
 {
     internal static class UnsupportedKSPAlertBox
     {
-        private static readonly string MSG = @"Unfortunately TweakScale is currently not known to work correctly on KSP {0}!
+        private static readonly string MSG = @"Unfortunately TweakScale is currently not known to work correctly on KSP {0} (and newer)!
 
 It's not certain that it will not work fine, it's **NOT KNOWN** and if anything goes wrong, KSP will inject bad information on your savegames corrupting parts with TwekScale.";
 
-        private static readonly string AMSG = @"download the latest release of TweakScale";
+        private static readonly string AMSG = @"check the GitHub issue #{1} (KSP will close) to be updated on any news about KSP {0}";
 
-        internal static void Show(string currentVersion)
+        internal static void Show(string kspVersion, string issueNumber)
         {
             KSPe.Common.Dialogs.ShowStopperAlertBox.Show(
-                string.Format(MSG, currentVersion),
+                string.Format(MSG, kspVersion, issueNumber),
                 AMSG,
-                () => { Application.OpenURL("https://github.com/net-lisias-ksp/TweakScale/releases"); Application.Quit(); }
+                () => { Application.OpenURL(string.Format("https://github.com/net-lisias-ksp/TweakScale/issues/{0}", issueNumber)); Application.Quit(); }
             );
-            Log.force("\"Houston, we have a Problem!\" about KSP 1.10.1 was displayed");
+            Log.force("\"Houston, we have a Problem!\" about KSP {0} was displayed", kspVersion);
         }
     }
 }
