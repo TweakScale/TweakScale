74ee6fd41aa7da282cd9efafd5008bc09cc6a2bc
Lisias T <me@lisias.net>
2021-09-27 03:43:21 -0300

    Small mistakes fixed.

--- Source/PartDB13x/Startup.cs
+++ Source/PartDB13x/Startup.cs
@@ -21,14 +21,14 @@
 		You should have received a copy of the GNU General Public License 2.0
 		along with TweakScale /L If not, see <https://www.gnu.org/licenses/>.
 */
-using UnityEngine;
+
 namespace TweakScale.PartDB.KSP13
 {
 	public class Startup
 	{
 		private void Start()
 		{
-			UnityEngine.Debug.LogFormat("Support for KSP 1.3.0 to 1.3.1 Version {0}", Version.Text);
+			Log.force("Support for KSP 1.3.0 to 1.3.1 Version {0}", Version.Text);
 		}
 	}
 }
