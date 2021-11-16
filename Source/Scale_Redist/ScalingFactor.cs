ef0853abe2b4b5c4790b158a4f78de4920005cb1
Lisias T <me@lisias.net>
2021-11-16 04:14:15 -0300

    Better whitespacing on the license headers

--- Source/Scale_Redist/ScalingFactor.cs
+++ Source/Scale_Redist/ScalingFactor.cs
@@ -1,19 +1,18 @@
 ﻿/*
-		This file is part of TweakScale /L
+	This file is part of TweakScale /L
 		© 2018-2021 LisiasT
 		© 2015-2018 pellinor
 		© 2014 Gaius Godspeed and Biotronic
 
-		THIS FILE is licensed to you under:
-
+	THIS FILE is licensed to you under:
 		* WTFPL - http://www.wtfpl.net
 			* Everyone is permitted to copy and distribute verbatim or modified
- 			    copies of this license document, and changing it is allowed as long
+ 				copies of this license document, and changing it is allowed as long
 				as the name is changed.
 
-		THIS FILE is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	THIS FILE is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 */
 namespace TweakScale
 {
--- Source/Scale_Redist/Updater.cs
+++ Source/Scale_Redist/Updater.cs
@@ -1,19 +1,24 @@
 ﻿/*
-		This file is part of TweakScale /L
+	This file is part of TweakScale /L
 		© 2018-2021 LisiasT
 		© 2015-2018 pellinor
 		© 2014 Gaius Godspeed and Biotronic
 
-		THIS FILE is licensed to you under:
+	TweakScale /L is double licensed, as follows:
+		* SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+		* GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
 
-		* WTFPL - http://www.wtfpl.net
-			* Everyone is permitted to copy and distribute verbatim or modified
- 			    copies of this license document, and changing it is allowed as long
-				as the name is changed.
+	And you are allowed to choose the License that better suit your needs.
 
-		THIS FILE is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	TweakScale /L is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the SKL Standard License 1.0
+	along with TweakScale /L. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with TweakScale /L If not, see <https://www.gnu.org/licenses/>.
 */
 namespace TweakScale
 {
