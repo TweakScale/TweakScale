ad011c85b27d0cb08a57a89d3aef1a02ce548f04
Lisias T <me@lisias.net>
2022-07-10 02:18:25 -0300

    Implements a FIX for BlueDog_DB and a CHECK for Tantares.

--- Source/Tests/Program.cs
+++ Source/Tests/Program.cs
@@ -7,11 +7,13 @@ namespace Tests
 		public static void Main(string[] args) {
 			Console.WriteLine("Hello World!");
 			EscapeDataString("https://ksp.lisias.net/add-ons/TweakScale/Support/SanityChecks/B9PartSwitch-Conflicts");
-			EscapeDataString("https://ksp.lisias.net/add-ons/TweakScale/Support/SanityChecks/ConfigurableContainers");
 			EscapeDataString("https://ksp.lisias.net/add-ons/TweakScale/Support/SanityChecks/Firespitter-Buoyancy");
 			EscapeDataString("https://github.com/net-lisias-ksp/TweakScaleCompantion_FS/issues/1");
 			EscapeDataString("https://github.com/net-lisias-ksp/TweakScale/issues/12");
 			EscapeDataString("https://github.com/net-lisias-ksp/TweakScaleCompantion_FuelSwitches/issues/2");
+			EscapeDataString("https://ksp.lisias.net/add-ons/TweakScale/Support/SanityChecks/Configurable-Containers-Needs-Companion");
+			EscapeDataString("https://ksp.lisias.net/add-ons/TweakScale/Support/SanityChecks/BlueDog_DB-Needs-Companion");
+			EscapeDataString("https://ksp.lisias.net/add-ons/TweakScale/Support/SanityChecks/Tantares-Needs-Companion");
 		}
 
 		private static void EscapeDataString(string v) {
