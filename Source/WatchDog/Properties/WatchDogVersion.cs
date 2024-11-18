4f3bc49ad789dd013e0151c0340a27ed228654eb
Lisias T <me@lisias.net>
2024-11-17 23:05:47 -0300

    Fixing a borkage on the WatchDog versioning!

---
+++ Source/WatchDog/Properties/WatchDogVersion.cs
@@ -0,0 +1,12 @@
+﻿namespace TweakScale.WatchDog
+{
+	public static class Version
+	{
+		public const int major = TweakScale.Version.major;
+		public const int minor = TweakScale.Version.minor;
+		public const int patch = TweakScale.Version.patch;
+		public const int build = TweakScale.Version.build;
+		public const string Number = TweakScale.Version.Number;
+		public const string Text = TweakScale.Version.Text;
+	}
+}
